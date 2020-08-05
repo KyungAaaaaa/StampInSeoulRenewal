dae7436dedcc4e609868cfc3912c382fd20f587c
yung34 <yung3455@naver.com>
2020-08-05 09:50:51 +0900

    시작화면 수정

--- StampInSeoul2/app/src/main/java/com/example/stampinseoul2/StartActivity.java
+++ StampInSeoul2/app/src/main/java/com/example/stampinseoul2/StartActivity.java
@@ -1,13 +1,63 @@
 package com.example.stampinseoul2;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class StartActivity extends Activity {
+
+    //어플 시작할 때 로딩 제공
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start);
+
+        getHashKey();
+
+        try{
+            Thread.sleep(2500);
+        }catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+
+    }
+
+    private void getHashKey() {
+        PackageInfo packageInfo = null;
+
+        try{
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch(PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if(packageInfo == null) {
+            Log.e("KeyHash","keyHash,:null");
+
+
+            for (Signature signature : packageInfo.signatures) {
+                try {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                } catch (NoSuchAlgorithmException e) {
+                    Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+                }
+            }
+        }
+
     }
 }
