b8abc4228459d505c9e2efd8ba401c3a557e6c0d
gon00130 <gon00130@gmail.com>
2020-08-07 01:01:07 +0900

    앨범기능추가 && 플로팅버튼오류수정중

--- StampInSeoul2/app/src/main/java/com/example/stampinseoul2/AlbumAdapter.java
+++ StampInSeoul2/app/src/main/java/com/example/stampinseoul2/AlbumAdapter.java
@@ -1,4 +1,68 @@
 package com.example.stampinseoul2;
 
-public class AlbumAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.CustomViewHolder> {
+
+    private int layout;
+    private ArrayList<ThemeData> list;
+
+    public AlbumAdapter(int layout, ArrayList<ThemeData> list) {
+        this.layout = layout;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public AlbumAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(layout, viewGroup, false);
+        CustomViewHolder viewHolder = new CustomViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AlbumAdapter.CustomViewHolder holder, int position) {
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class CustomViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imgReview;
+        public de.hdodenhof.circleimageview.CircleImageView imgChoice;
+
+        public TextView txtPola;
+        public TextView txtTitle;
+        public TextView txtContent;
+        public TextView txtID;
+
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgReview = itemView.findViewById(R.id.imgReview);
+            imgChoice = itemView.findViewById(R.id.imgChoice);
+
+            txtPola = itemView.findViewById(R.id.txtPola);
+            txtTitle = itemView.findViewById(R.id.txtTitle);
+            txtContent = itemView.findViewById(R.id.txtContent);
+            txtID = itemView.findViewById(R.id.txtID);
+        }
+    }
+
+    private class CircleImageView {
+    }
 }
+
